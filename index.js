6619bb3a206733eb138dc31ba51a15109ea0d62c
Harry Ruiz <harryyadier@gmail.com>
2023-08-06 10:58:13 -0400

    fixed responsiveness and fixed DEL button.

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const buttons = document.getElementsByClassName("buttons");
 const cero = document.getElementById("0");
 const clear = document.getElementById("clear");
 const equal = document.getElementById("equal");
+const toNeg = document.getElementById("toNeg"); 
+const DEL = document.getElementById("DEL");
+
+function clearInput() {
+    inputCal.value = ""; 
+}
 
 for (let n=0; n<numberButtons.length; n++) {
     numberButtons[n].onclick = function() {
@@ -30,6 +36,25 @@ equal.onclick = function() {
         inputCal.value = eval(inputCal.value); 
     } catch (error) {
         inputCal.value = error;
+        setTimeout(clearInput, 1500); 
     }
 }
+
+toNeg.onclick = function() {
+    let oldInput = inputCal.value; 
+    inputCal.value = "This doesn't work, dont press it!"; 
+    setTimeout(function() {
+        inputCal.value = oldInput; 
+    }, 1000); 
+
+}
+
+DEL.onclick = function() {
+    let inputArray = inputCal.value.split("");
+    console.log(inputArray);
+    inputArray = inputArray.slice(0, -1);
+    console.log(inputArray);
+    inputCal.value = inputArray.join("");
+}
+
 
